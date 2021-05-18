2dc97dd2c7347a9a1fc1f215a8577468326517fa
SJasper <64356366+SJasper@users.noreply.github.com>
2021-05-18 14:24:34 +0200

    reset init.py for use with py2

--- joint_control/keyframes/__init__.py
+++ joint_control/keyframes/__init__.py
@@ -1,11 +1,11 @@
 '''Keyframes from Aldebaran
 '''
 
-from .leftBackToStand import leftBackToStand
-from .leftBellyToStand import leftBellyToStand
-from .rightBackToStand import rightBackToStand
-from .rightBellyToStand import rightBellyToStand
-from .dance import dance
+from leftBackToStand import leftBackToStand
+from leftBellyToStand import leftBellyToStand
+from rightBackToStand import rightBackToStand
+from rightBellyToStand import rightBellyToStand
+from dance import dance
 
-from .hello import hello
-from .wipe_forehead import wipe_forehead
+from hello import hello
+from wipe_forehead import wipe_forehead
